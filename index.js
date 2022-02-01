2bbaac2c1578eeaccd0e58f727cc2ec083f48188
pep89 <pep3689@gmail.com>
2022-02-01 12:36:43 +0100

    ajustes para deployment 4

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.use('/api/tareas', require('./routes/tareas'));
 
 //arrancar la app
 app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor funciona en el puerto ${PORT}`);
+    console.log(`El servidor funciona en el puerto ${port}`);
 });
